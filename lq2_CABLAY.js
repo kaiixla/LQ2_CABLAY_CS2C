8eb88a5cf8f1fabbfd1e3e90823965d9e4dc4f89
kaiixla <124885911+kaiixla@users.noreply.github.com>
2024-10-06 00:53:38 +0800

    initial commit

--- lq2_CABLAY.js
+++ lq2_CABLAY.js
@@ -30,18 +30,18 @@ if(average >= 85) {
 //II. Code Snippet
 //adding this prompt-sync to prompt the username and passcode
 const ps = require("prompt-sync");
-const prompt = ps();
+const kprompt = ps();
 
 let passcode = "ilovedsa";
 let username = "kaiixla";
-const usName = prompt("Register your Username: ");//declare a var that will hold the uname, use a user input method
-const pCode  = prompt("Register your passcode: "); //declare a var that will hold the pword, use a user input methodc
-const repCode = prompt("Re-enter your passcode: "); //declare a variable that will hold the re-pword, use a user input method
+const usName = kprompt("Register your Username: ");//declare a var that will hold the uname, use a user input method
+const pCode  = kprompt("Register your passcode: "); //declare a var that will hold the pword, use a user input methodc
+const repCode = kprompt("Re-enter your passcode: "); //declare a variable that will hold the re-pword, use a user input method
     if (pCode === passcode) { //compare the Registered Password and Re-entered Password
         console.log("Congratulations! You entered a correct passcode."); //window Pop-up to Congratulate the user for registering.
-            const uName = prompt("Username: "); //declare a variable that will hold the uname on LogIn
+            const uName = kprompt("Username: "); //declare a variable that will hold the uname on LogIn
                 if (username === uName){ //compare the registered uname and entered uname on LogIn
-                    const paCode = prompt("Password: "); //if correct uname, declare a var that will hold the encoded password on LogIn
+                    const paCode = kprompt("Password: "); //if correct uname, declare a var that will hold the encoded password on LogIn
                 (paCode === passcode) //compare the registered pword and entered pword on LogIn
                     ? console.log("Welcome to DSA, " + username): //window Pop-up to Congratulate the user for successful LogIn.
                     console.log("Incorrect Password, Re-run the code!"); //window Pop-up to Inform the user of the incorrect passcode    
@@ -57,3 +57,102 @@ const repCode = prompt("Re-enter your passcode: "); //declare a variable that wi
 //adding this prompt-sync to prompt the username and passcode
 const ps = require("prompt-sync");
 const prompt = ps();             
+
+//making a variables for the class roles
+let Officer = "Officer";
+let Student = "Student";
+let Teacher = "Teacher";
+
+//prompting the name, address, age, class roles, and course
+let kName = prompt("Enter your name: ");
+let kAddress = prompt("Enter your address: ");
+let kAge = parseInt(prompt("Enter your age: "));
+let kCRoles = prompt("Enter your class role (Officer, Student, Teacher): ");
+let kCourse = prompt("Enter your course (BSCS, BSM, BAEL): ");
+
+//using statements to match the cases with the specific value
+switch (kCourse) {
+    //case for BSCS Course
+    case "BSCS":
+        alert("You are enrolled in Bachelor of Science in Computer Science.");
+
+        if (kCRoles === Officer) {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            alert(kName + " - " + Officer);
+            }
+        } else if (kCRoles === "Student") {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            alert(kName + " - " + Student);
+            }
+        } else if (kCRoles === "Teacher") {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            alert(kName + " - " + Teacher);
+            }
+        } else {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            //if the student input a non-exsistent class role it will display the default role which is the student
+            alert(kName + " - Student");
+            }
+        }
+        break;
+        //case for BSM course
+    case "BSM":
+        alert("You are enrolled in Bachelor of Science in Midwifery.");
+
+        if (kCRoles === Officer) {
+            for (let i = 0; i < kAge / 4; i++)
+{
+                alert(kName + " - " + Officer);
+            }
+        } else if (kCRoles === "Student") {
+            for (let i = 0; i < kAge / 4; i++)
+{
+                alert(kName + " - " + Student);
+            }
+        } else if (kCRoles === "Teacher") {
+            for (let i = 0; i < kAge / 4; i++)
+{
+                 alert(kName + " - " + Teacher);
+            }
+        } else {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            //if the student input a non-exsistent class role it will display the default role which is the student
+            alert(kName + " - Student");
+            }
+        }
+        break;
+        //case for BAEL course
+    case "BAEL":
+        alert("You are enrolled in Bachelor of Arts in English Language.");
+
+        if (kCRoles === Officer) {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            alert(kName + " - " + Officer);
+            }
+        } else if (kCRoles === "Student") {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            alert(kName + " - " + Student);
+            }
+        } else if (kCRoles === "Teacher") {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            alert(kName + " - " + Teacher);
+            }
+        } else {
+            for (let i = 0; i < kAge / 4; i++)
+{
+            //if the student input a non-exsistent class role it will display the default role which is the student
+            alert(kName + " - Student");
+            }
+        }
+        break;
+    default:
+            alert(kName + " Course not recognized. Please enter BSCS, BSM or BAEL.");
+}
